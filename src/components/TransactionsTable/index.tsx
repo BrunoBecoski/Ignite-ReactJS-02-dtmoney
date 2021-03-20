7e55e2e205032e3dec94d5e25620d5d65a0718b7
Bruno Becoski <brunobecoski@gmail.com>
2021-03-19 22:52:03 -0300

    Contextos e hooks

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,23 +1,8 @@
-import { useEffect, useState } from 'react';
-import { api } from '../../services/api';
+import { useTransactions } from '../../hooks/useTransactions';
 import { Container } from "./styles";
 
-interface Transaction {
-  id: number;
-  title: string;
-  amount: number;
-  type: string;
-  category: string;
-  createAt: string;
-}
-
 export function TransactionsTable() {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
-
-  useEffect(() => {
-    api.get('transactions')
-      .then(response => setTransactions(response.data.transactions));
-  }, []);
+  const { transactions } = useTransactions();
 
   return(
     <Container>
@@ -36,14 +21,17 @@ export function TransactionsTable() {
             <tr key={transaction.id}>
               <td>{transaction.title}</td>
               <td className={transaction.type}>
-                 {new Intl.NumberFormat('pt-BR',{
-                   style: 'currency',
-                   currency: 'BRL',
-                 }).format(transaction.amount)}</td>
+                { transaction.type === 'deposit' ? '' : '- '}
+                {new Intl.NumberFormat('pt-BR',{
+                    style: 'currency',
+                    currency: 'BRL',
+                  }).format(transaction.amount)
+                }
+              </td>
               <td>{transaction.category}</td>
               <td>
                 {new Intl.DateTimeFormat('pt-BR').format(
-                  new Date(transaction.createAt)
+                  new Date(transaction.createdAt)
                 )}
               </td>
             </tr>
